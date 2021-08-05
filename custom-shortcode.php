56bb6140c4ed6a4700ee3c76a62e3cc9b649e7a8
Rupesh Prabhu <rupeshprabhu@outlook.com>
2021-08-06 01:44:22 +0530

    PHP header comment added  test funtion for it

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -1,2 +1,19 @@
-/* */
-php echo '<p>Hello World</p>'
+<?php
+/**
+ * Undocumented function
+ * Plugin Name: Custom ShortCode
+ * Plugin URI: https://github.com/rupeshprabhu/custom-shortcode
+ * Description: A Custom short code plugin to showcase the recent 5 posts in wordpress
+ * Author: Rupesh Prabhu
+ * Version: 1.0
+ * Author URI: https://github.com/rupeshprabhu
+ * Text Domain: shortcoder
+ * Domain Path: /languages
+ *
+ * @since 0.0.0
+ */
+function dotiavatar_function() {
+    /** funtion for test php file to return the image */
+    return '<img src="http://dayoftheindie.com/wp-content/uploads/avatar-simple.png" alt="doti-avatar" width="96" height="96" class="left-align" />';
+}
+
