c77c80fcc40872c8f09fd2bafb0cc5fb6c340ee0
Rupesh Prabhu <rupeshprabhu@outlook.com>
2021-08-07 14:32:34 +0530

    Adding Query for the latest post

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -14,6 +14,14 @@
  */
 function custom_shortcode_posts() {
     /** funtion for test php file to return the image */
+    $args = array(
+    'post_status' => 'publish',
+    'posts_per_page' => 5,
+    'orderby' => 'post_date',
+    'order' => 'DESC',);
+    $latest_post = new WP_Query( $args );
+
+    wp_reset_postdata();
     return '<img src="http://dayoftheindie.com/wp-content/uploads/avatar-simple.png" alt="doti-avatar" width="96" height="96" class="left-align" />';
 }
 
