f7a222c8c122622a26dd16823c38dfb52338c1a2
Rupesh Prabhu <rupeshprabhu@outlook.com>
2021-08-10 00:31:37 +0530

    Updating the loop and to showcase the header

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -3,7 +3,7 @@
  * Undocumented function
  * Plugin Name: Custom ShortCode
  * Plugin URI: https://github.com/rupeshprabhu/custom-shortcode
- * Description: A Custom short code plugin to showcase the recent 5 posts in wordpress
+ * Description: Display a listing of posts using the [custom-shortcode] shortcode
  * Author: Rupesh Prabhu
  * Version: 1.0
  * Author URI: https://github.com/rupeshprabhu
@@ -12,17 +12,23 @@
  *
  * @since 0.0.0
  */
+add_shortcode('custom-shortcode','custom_shortcode_posts');
 function custom_shortcode_posts() {
     /** funtion for test php file to return the image */
     $args = array(
     'post_status' => 'publish',
     'posts_per_page' => 5,
     'orderby' => 'post_date',
-    'order' => 'DESC',);
-    $latest_post = new WP_Query( $args );
-
-    wp_reset_postdata();
-    return '<img src="http://dayoftheindie.com/wp-content/uploads/avatar-simple.png" alt="doti-avatar" width="96" height="96" class="left-align" />';
+    'order' => 'DESC',); //Conditional Statements for latest posts
+    $latest_post = new WP_Query( $args ); //Creating WP Query
+    if($latest_post->have_posts()){ //checking if we have posts
+        while($latest_post->have_posts()){ //loop through all the posts
+            $latest_post->the_post();//increamenting the posts
+            echo '<h2>'.get_the_title().'</h2>';
+        }
+        wp_reset_postdata(); //restore our original posts data
+    }
+    else{
+        //no posts
+    }
 }
-
-add_shortcode('custom-shortcode','custom_shortcode_posts');
