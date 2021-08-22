a82b03120f094ae3f9f9fe2b5272ff8f00565391
Rupesh Prabhu <rupeshprabhu@outlook.com>
2021-08-22 18:35:31 +0530

    Updating the code as per coding standards.

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -11,26 +11,26 @@
  * Domain Path: /languages
  *
  * @since 0.0.0
+ * @package category
  */
-add_shortcode('custom-shortcode','custom_shortcode_posts');
+
+add_shortcode( 'custom-shortcode', 'custom_shortcode_posts' );
+/**  Function for the custom shortcode to display recent posts. */
 function custom_shortcode_posts() {
-    /** funtion for test php file to return the image */
-    $args = array(
-    'post_status' => 'publish',
-    'posts_per_page' => 5,
-    'orderby' => 'post_date',
-    'order' => 'DESC',); //Conditional Statements for latest posts
-    $latest_post = new WP_Query( $args ); //Creating WP Query
-    if($latest_post->have_posts()){ //checking if we have posts
-        while($latest_post->have_posts()){ //loop through all the posts
-            $latest_post->the_post();//increamenting the posts
-            echo '<h2><a href='.get_the_permalink().'>'.get_the_title().'</h2></a>';
-            echo '<p><a href='.get_the_permalink().'>'.get_the_post_thumbnail().'</a></p>';
-            echo '<p>'.the_excerpt(__('(more…)')).'</p>';
-        }
-        wp_reset_postdata(); //restore our original posts data
-    }
-    else{
-        //no posts
-    }
+						$args        = array(
+							'post_status'    => 'publish',
+							'posts_per_page' => 5,
+							'orderby'        => 'post_date',
+							'order'          => 'DESC',
+						);
+						$latest_post = new WP_Query( $args ); // Creating WP Query.
+						if ( $latest_post->have_posts() ) { // Checking if we have posts.
+							while ( $latest_post->have_posts() ) { // loop through all the posts.
+								$latest_post->the_post(); // increamenting the posts.
+								echo '<h2><a href = ' .get_the_permalink(). '>' .get_the_title().' </h2></a> ';
+								echo '<p><a href='.get_the_permalink().'>'.get_the_post_thumbnail().'</a></p>';
+								echo '<p>'.the_excerpt(__('(more…)')).'</p>';
+							}
+							wp_reset_postdata(); // restore our original posts data.
+						}
 }
